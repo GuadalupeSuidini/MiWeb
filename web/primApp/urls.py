0889ff48f844856ffc17690ec5090ca101012dac
Heiner Alexander Millan <99698661+Alexander0599@users.noreply.github.com>
2022-08-16 21:25:50 -0500

    datos nuevos

--- web/primApp/urls.py
+++ web/primApp/urls.py
@@ -1,7 +1,18 @@
 
 from django.urls import path
-from .views import inicio
+from .views import inicio, ingreso_usuarios, registro
 
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
-    path('', inicio),
+    path('', inicio, name = 'inicio'),
+
+
+############ url ingreso usuarios
+    path('login',ingreso_usuarios, name = 'login'),
+    path('registar',registro, name = 'registrar'),
+    path('logout/',LogoutView.as_view(template_name='logout.html'), name="logout"),
+
+
+
+
 ]
--- web/primApp/views.py
+++ web/primApp/views.py
@@ -1,8 +1,64 @@
+from multiprocessing import AuthenticationError
 from django.shortcuts import render
-
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
 def inicio(request):
     return render(request,"padre.html")
-    
+###
+# ingreso y registro de ususarios   
+def ingreso_usuarios(request):
+
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+
+            user = data["username"]
+            passw = data["password"]
+
+            ingreso = authenticate(username=user, password=passw)
+
+            if ingreso:
+                login(request, ingreso)
+
+                return render(request,"padre.html", {"mensaje": f"Bienvenido {user}"})
+
+            else:
+
+                return render(request,"erroringreso.html", {"mensaje":"Error, Datos Incorrectos"})
+        return render(request,"erroringreso.html", {"mensaje":"Error, Datos Incorrectos"})
+    else: 
+
+        form = AuthenticationForm()
+
+        return render(request,"ingreso.html", {'form':form})
+
+def registro(request):
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+
+            username = form.cleaned_data["username"]
+            form.save()
+            return render (request, "padre.html", {"mensaje": f'Usuario {username} creado'})
+
+    else:
+
+        form = UserCreationForm()
+
+    return render(request, "registro.html", {"form": form})
+
+
+
+
+
+
+######################################################################################################################
